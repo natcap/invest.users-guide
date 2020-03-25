0bf65d1f61076224a28aa83375565dcff14aafca
James Douglass <jamesdouglassusa@gmail.com>
2020-03-25 12:20:33 -0700

    Adding a description, shebang to script and correcting path.

--- .github/workflows/match-invest-tags.sh
+++ .github/workflows/match-invest-tags.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#
+# Match tags from InVEST.
+#
+# Loops through all known tags in InVEST and checks to see if the
+# tag is present in the UG git repo.  If it isn't present, we'll
+# create it based on what's in the target rev's Makefile.
+
 for InVEST_TAG in `git -C invest-src tag -l`
 do
     if [[ `git -C . tag -l $InVEST_TAG` == $InVEST_TAG ]]

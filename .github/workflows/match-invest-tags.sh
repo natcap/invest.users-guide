6beb86bf6b9e7a88961237b92aeff7e2415ead52
James Douglass <jamesdouglassusa@gmail.com>
2020-03-25 12:10:48 -0700

    Adding a workflow to compare tags against InVEST.

---
+++ .github/workflows/match-invest-tags.sh
@@ -0,0 +1,12 @@
+for InVEST_TAG in `git -C invest-src tag -l`
+do
+    if [[ `git -C . tag -l $InVEST_TAG` == $InVEST_TAG ]]
+    then
+        echo "Already present in user's guide:" $InVEST_TAG
+    else
+        git -C invest-src checkout $InVEST_TAG
+        UG_REV=$(grep ^GIT_UG_REPO_REV invest-src/Makefile | awk -F ' ' '{ print $3 }')
+        echo "Adding tag $InVEST_TAG for userguide rev $UG_REV (from InVEST Makefile)"
+        git -C . tag $InVEST_TAG $UG_REV
+    fi
+done

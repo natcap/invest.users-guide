18149ad50defc9190205996733d15edc5d25763d
James Douglass <jamesdouglassusa@gmail.com>
2020-03-25 12:40:24 -0700

    Pushing any created tags back to the UG.

--- .github/workflows/match-invest-tags.sh
+++ .github/workflows/match-invest-tags.sh
@@ -16,5 +16,12 @@ do
         UG_REV=$(grep ^GIT_UG_REPO_REV invest-src/Makefile | awk -F ' ' '{ print $3 }')
         echo "Adding tag $InVEST_TAG for userguide rev $UG_REV (from InVEST Makefile)"
         git -C . tag $InVEST_TAG $UG_REV
+
+        if [[ ! -z "$GITHUB_TOKEN" ]]
+        then
+            git push tags https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git $InVEST_Tag
+        else
+            echo "GITHUB_TOKEN not defined, skipping push."
+        fi
     fi
 done

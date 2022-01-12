411e762077bfbf2479c1ceb4fa66544660ef4330
emlys <esoth@stanford.edu>
2022-01-12 15:12:59 -0800

    remove demo_investspec, add integration test

--- extensions/investspec/test/test_investspec.py
+++ extensions/investspec/test/test_investspec.py
@@ -1,11 +1,28 @@
+import os
+import shutil
+import subprocess
 import unittest
 from unittest.mock import MagicMock
 
 import investspec
 from docutils.nodes import emphasis, Node, reference, strong
 
+TEST_DIR = os.path.abspath(os.path.dirname(__file__))
+BUILD_DIR = os.path.join(TEST_DIR, 'build')
+
 
 class TestInvestSpec(unittest.TestCase):
+
+    @classmethod
+    def beforeAll(cls):
+        """Install mock module."""
+        subprocess.run([
+            'pip', 'install', os.path.join(TEST_DIR, 'test_module')])
+
+    @classmethod
+    def afterAll(cls):
+        """Remove mock module build directory."""
+        shutil.rmtree(BUILD_DIR)
 
     def test_parse_rst(self):
         """parse_rst should create a correct list of docutils nodes."""
@@ -32,13 +49,29 @@ class TestInvestSpec(unittest.TestCase):
         mock_inliner.document.settings.env.app.config.language = 'en'
         nodes, messages = investspec.invest_spec(
             None, None, 'test_module number_input', None, mock_inliner)
-        print(nodes)
         self.assertEqual(len(nodes), 8)
         for node in nodes:
             self.assertTrue(isinstance(node, Node))
         self.assertEqual(messages, [])
 
+    def test_investspec_integration(self):
+        """Built html should contain generated arg documentation."""
+
+        subprocess.run([
+            'sphinx-build',
+            '-W',  # fail on warning
+            '-a',  # write all files, not just new or changed files
+            '-b', 'html',  # build html
+            TEST_DIR, BUILD_DIR])
+        expected_html = (
+            '<strong>Foo</strong> (<a class="reference external" '
+            'href="input_types.html#number">number</a>, units: '
+            '<strong>m³/month</strong>, <em>required</em>): Numbers have '
+            'units that are displayed in a human-readable way.')
+        with open(os.path.join(BUILD_DIR, 'index.html')) as file:
+            actual_html = file.read()
+        self.assertIn(expected_html, actual_html)
+
 
 if __name__ == '__main__':
     unittest.main()
-
